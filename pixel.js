e2dde9aaaf4866e7b2a70af369f6b4b9dbf9fb37
Salva Camarasa <salvacms@gmail.com>
2022-08-10 17:51:43 +0200

    Added UI buttons, color change, grid scaling and resize handler

--- pixel.js
+++ pixel.js
@@ -1,56 +1,134 @@
+'use strict';
+
 window.onload = (ev) => {
+  //Handler for the canvas operations
   const handleCanvasClick = (ev) => {
+    //Get absolute position respect to canvas
     const x = ev.clientX - mainCanvas.canvasElement.offsetLeft;
     const y = ev.clientY - mainCanvas.canvasElement.offsetTop;
-    console.log(x,y);
-    let foundCellIndex = mainCanvas.grid.findIndex(cell => {
-      return (
-        x <= (cell.posX + cell.width) && y <= (cell.posY + cell.height)
-        );
-    });
-    if (foundCellIndex >= 0) {
-      mainCanvas.grid[foundCellIndex].fillColor = "#000";
-      mainCanvas.drawGrid();
+
+    //We only have two tools (brush and bucket) so a simple else/if suffice
+    if (selectedTool == "pencil") {
+      let foundCellIndex = mainCanvas.grid.findIndex(cell => {
+        return (
+          x <= (cell.posX + cell.width) && y <= (cell.posY + cell.width)
+          );
+      });
+      if (foundCellIndex >= 0) {
+        mainCanvas.grid[foundCellIndex].fillColor = mainCanvas.selectedColor;
+        mainCanvas.drawGrid();
+      }
+    } else {
+        //TODO: bucket painting tool logic
     }
   }
 
-  const mainCanvas = new MainCanvas("main-canvas", 8);
-  mainCanvas.canvasElement.addEventListener("click", handleCanvasClick, false);
+  //Set the brush/bucket color
+  const handlePaletteColorChange = (ev) => {
+    mainCanvas.selectedColor = ev.detail.color;
+  }
+
+  //When window resizes, readapt and redraw everything
+  const handleWindowResize = () => {
+    //TODO: set wrapper w/h, as with some sizes this comes a bit unbereable to use
+    mainCanvas.width = document.querySelector("#wrapper").offsetWidth;
+    mainCanvas.height = document.querySelector("#wrapper").offsetWidth;
+    mainCanvas.canvasElement.width = document.querySelector("#wrapper").offsetWidth;
+    mainCanvas.canvasElement.height = document.querySelector("#wrapper").offsetWidth;
+    mainCanvas.updateGrid();
+    mainCanvas.drawGrid();
+  }
+
+  //Grid resolution size change
+  const handleGridSize = (ev) => {
+    if (mainCanvas.gridSize != ev.target.dataset.gridSize) {
+      mainCanvas.gridSize = ev.target.dataset.gridSize;
+      mainCanvas.updateGrid(true);
+      mainCanvas.drawGrid();
+    }
+  }
+  
+  //Init palette and main drawing canvas
+  const palette = createPalette("#row-palette");
+  const mainCanvas = new MainCanvas("#main-canvas", 8);
+  const sizeControls = document.querySelectorAll("#col-controls-size-select button");
+  const selectedTool = "pencil";
+
+  //Add eventListeners to our elements
+  mainCanvas.canvasElement.addEventListener("click", handleCanvasClick, false);  
+  palette.addEventListener("colorChange", handlePaletteColorChange, false);
+  sizeControls.forEach(element => {
+    element.addEventListener("click", handleGridSize, false);
+  });
+  window.addEventListener("resize", handleWindowResize, false);  
 }
 
 function MainCanvas (domElementId, gridSize) {
-  this.domElementId = domElementId || "canvas";
-  this.gridSize = gridSize || 8;
-  this.width = 400;
-  this.height = 400;
-  this.gridBorderColor = "#000";
-  this.canvasElement = document.getElementById(this.domElementId);
+  this.domElementId = domElementId || "#canvas";
+  this.canvasElement = document.querySelector(this.domElementId);
   this.context = this.canvasElement.getContext('2d');
+  this.gridSize = gridSize || 8;
+  this.width = document.querySelector("#wrapper").offsetWidth;
+  this.height = document.querySelector("#wrapper").offsetWidth;
+  this.gridBorderColor = "#000";
   this.grid = [];
+  this.selectedColor = "#000";
 
   this.init = function() {
     this.canvasElement.width = this.width;
     this.canvasElement.height = this.height;
+
     this.createGrid = function() {
       for (let i = 0; i < this.gridSize; i++) {
         for (let j = 0; j < this.gridSize; j++) {
           this.grid.push(new Cell(
-            Math.ceil(this.width / this.gridSize), 
-            Math.ceil(this.height / this.gridSize),
-            (Math.ceil(this.width / this.gridSize) * j) % this.width,
-            (Math.ceil(this.height / this.gridSize * i)) % this.height)
-            );
+            this.width / this.gridSize, 
+            this.width / this.gridSize,
+            ((this.width / this.gridSize * j) % this.width),
+            ((this.width / this.gridSize * i) % this.width)
+          ))
         }
       }
     }
 
-    this.getGrid = () => this.grid;
+    //Scaling function when window is resized
+    this.updateGrid = function(resizeDensity) {
+      if (resizeDensity) {
+        /*
+         * By now, let's just erase the current drawing. While it'd possible 
+         * to save and set the already drawn pixels if the user wants to 
+         * upscale the resolution (via matrix transpose for instance),
+         * it'd be a bit more complex to go the other way (downscaling), as 
+         * some kind of interpolation should be done in order to keep the most
+         * significant information after decreasing the density. This is not
+         * a requirement, so i'll keep the most simple implementation.
+         */
+        const userAcceptsDelete = 
+          confirm("Warning, changing the grid density will erase the current drawing, are you sure?");
+        if (userAcceptsDelete) {
+          this.grid = [];
+          this.createGrid();
+        }
+      } else {
+        let yIndex = 0;
+        this.grid.forEach((cell, index) => {
+          if (index !== 0 && index % this.gridSize == 0) {
+            yIndex++;
+          }
+          cell.width = this.width / this.gridSize; 
+          cell.height = this.width / this.gridSize;
+          cell.posX = ((this.width / this.gridSize * (index % this.gridSize)) % this.width);
+          cell.posY = ((this.width / this.gridSize * yIndex)) % this.width;
+        });
+      }
+    }
 
+    //Grid (re)draw function
     this.drawGrid = () => this.grid.forEach(cell => {
       this.context.fillStyle = cell.fillColor;
       this.context.strokeStyle = this.gridBorderColor;
-      this.context.strokeRect(cell.posX, cell.posY, cell.width, cell.height);
-      this.context.fillRect(cell.posX, cell.posY, cell.width, cell.height);
+      this.context.strokeRect(cell.posX, cell.posY, cell.width, cell.width);
+      this.context.fillRect(cell.posX, cell.posY, cell.width, cell.width);
     });
 
     this.createGrid();
@@ -58,18 +136,63 @@ function MainCanvas (domElementId, gridSize) {
   }
 
   this.init();
-  console.log(this.grid);
 }
 
-function Cell (width, height, posX, posY) {
+//We assume it's a square cell
+function Cell (width, height, posX, posY, fillColor) {
   this.width = width || 0;
-  this.height = height || 0;
   this.posX = posX || 0;
   this.posY = posY || 0;
-  this.fillColor = "#FFF";
+  this.fillColor = fillColor || "#FFF";
 }
 
-function Palette () {
-  this.colors = [];
-  this.selectedColor = "0xFF";
+function createPalette (targetElementId) {
+  const colorPalette = {
+    default: [
+      "#0C46FA",
+      "#0BD9C6",
+      "#00F001",
+      "#FFF600",
+      "#FCA70D",
+      "#F50076",
+      "#5D00E5",
+      "#0D79FD",      
+      "#FFFFFF",
+      "#808080",
+      "#000000"
+    ]
+  };
+
+  const paletteWrapperElement = document.querySelector(targetElementId);
+  
+  //Color change handler
+  const handleColorChange = (ev) => {
+    const allColorsSelector = document.querySelectorAll(".palette-color");
+
+    //Remove all 'selected' classes
+    allColorsSelector.forEach(colorElement => {
+      colorElement.className = "palette-color";
+    });
+    //Add 'selected' class to the selected color
+    ev.target.className = "palette-color" + " selected";
+
+    const event = new CustomEvent("colorChange", {
+      detail: {
+        color: ev.target.dataset.color
+      }      
+    });
+    paletteWrapperElement.dispatchEvent(event);
+  }
+
+  //Create palette element
+  for (let i = 0; i < colorPalette.default.length; i++) {
+    const paletteColor = document.createElement('div');
+    paletteColor.className = "palette-color";
+    paletteColor.dataset.color = colorPalette.default[i];
+    paletteColor.style.backgroundColor = colorPalette.default[i];
+    paletteColor.addEventListener("click", handleColorChange, false);
+    paletteWrapperElement.appendChild(paletteColor);    
+  }
+
+  return paletteWrapperElement;
 }
